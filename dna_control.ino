8498d7f95103e40a00303cfc57dbec4720bc0d87
moon <moon93679983@gmail.com>
2025-02-09 16:18:17 +0900

    250209_13

--- dna_control.ino
+++ dna_control.ino
@@ -1,10 +1,10 @@
 void setup() {
-  pinMode(10, OUTPUT); // LED 핀 설정
+    pinMode(10, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(10, HIGH); // LED 켜기
-  delay(1000);            // 1초 대기
-  digitalWrite(10, LOW);  // LED 끄기
-  delay(1000);            // 1초 대기
+    digitalWrite(10, HIGH);  // LED 켜기
+    delay(1000);             // 1초 대기
+    digitalWrite(10, LOW);   // LED 끄기
+    delay(1000);
 }

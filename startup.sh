9b7adad999546ef7746516d8f9a4c1008d95a211
moon <moon93679983@gmail.com>
2025-02-11 22:15:58 +0900

    250211_2

--- startup.sh
+++ startup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# 작업 디렉토리로 이동 (절대 경로 사용)
+# 작업 디렉토리로 이동
 cd /home/aiseed/dna-control-system
 
-# 최신 코드 가져오기
-git pull origin main
+# 최신 코드 가져오기 (실패 시 계속 진행)
+git pull origin main || true
 
-# 파일 이동
+# 파일 이동 (에러 무시)
 mv -f index.html templates/ 2>/dev/null
 mv -f dna_control.ino dna_control/ 2>/dev/null
 

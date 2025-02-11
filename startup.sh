35ee51a751c964c4c081e6cd223de2c0169cd0a6
moon <moon93679983@gmail.com>
2025-02-11 22:09:37 +0900

    250211_1

--- startup.sh
+++ startup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# 작업 디렉토리로 이동
-cd ~/dna-control-system
+# 작업 디렉토리로 이동 (절대 경로 사용)
+cd /home/aiseed/dna-control-system
 
 # 최신 코드 가져오기
 git pull origin main
 
 # 파일 이동
-mv -f index.html templates/
-mv -f dna_control.ino dna_control/
+mv -f index.html templates/ 2>/dev/null
+mv -f dna_control.ino dna_control/ 2>/dev/null
 
 # Arduino 코드 업로드
-~/dna-control-system/upload_arduino.sh
+/home/aiseed/dna-control-system/upload_arduino.sh
 
 # 가상환경 활성화
-source venv/bin/activate
+source /home/aiseed/dna-control-system/venv/bin/activate
 
 # Flask 서버 실행
-python app.py
+/home/aiseed/dna-control-system/venv/bin/python app.py

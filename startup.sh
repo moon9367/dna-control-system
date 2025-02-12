e2132738f7e5a76a8d5e0a7dbf3dcac763d4026e
moon <moon93679983@gmail.com>
2025-02-13 00:30:04 +0900

    250213_7

--- startup.sh
+++ startup.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 # 최신 코드 가져오기
-cd ~/dna-control-system || exit
+cd /home/aiseed/dna-control-system || exit
 git pull origin main
 
 # 파일 이동
 mv -f index.html templates/ 2>/dev/null
 mv -f dna_control.ino dna_control/ 2>/dev/null
 
-
-~/dna-control-system/upload_arduino.sh
+/home/aiseed/dna-control-system/upload_arduino.sh
 
 # Flask 서버 실행 (백그라운드에서 실행)
-source ~/dna-control-system/venv/bin/activate
+source /home/aiseed/dna-control-system/venv/bin/activate
 python app.py &
 
 echo "✅ Flask 서버 실행 완료!"

fb738b23139f6b98607908b1b4fde837ebaf3acd
moon <moon93679983@gmail.com>
2025-02-12 23:40:12 +0900

    250212_2

--- send_ip_email.py
+++ send_ip_email.py
@@ -7,14 +7,20 @@ def get_ip():
     ip_address = os.popen("hostname -I").read().strip()
     return f"라즈베리파이 IP 주소: {ip_address}"
 
-with open("/home/aiseed/rc_local_log.txt", "a") as log_file:
-    log_file.write("send_ip_email.py script executed!\n")
+log_file_path = "/home/aiseed/rc_local_log.txt"
+
+# 로그 작성
+try:
+    with open(log_file_path, "a") as log_file:
+        log_file.write("send_ip_email.py script executed!\n")
+except Exception as e:
+    print(f"로그 작성 실패: {e}")
 
 
 # 이메일 전송 함수
 def send_email():
     sender_email = "tspol9983@gmail.com"  # 발신 Gmail 주소
-    app_password = "fksk leyb lheo nxzo"    # 앱 비밀번호
+    app_password = "fksk leyb lheo nxzo"  # 앱 비밀번호
     recipient_email = "moon93679983@gmail.com"  # 수신 이메일 주소
 
     # 이메일 내용 작성
